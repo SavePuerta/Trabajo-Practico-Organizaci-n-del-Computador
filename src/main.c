b4887cca8bbb163db499fc4f75977d8db0fa7c2b
SavePuerta <startaglia@fi.uba.ar>
2025-11-26 11:29:52 -0300

    fin 2

--- src/main.c
+++ src/main.c
@@ -58,7 +58,7 @@ int escribir_archivo_binario(const char* nombre, const unsigned char* data, size
 
 void codificar_archivo() {
     size_t input_size;
-    unsigned char* input_data = leer_archivo("codificacion.bin", &input_size);
+    unsigned char* input_data = leer_archivo("inputBinario.bin", &input_size);
     
     if (!input_data) {
         printf("Error: No se pudo leer inputBinario.bin\n");
@@ -91,7 +91,7 @@ void codificar_archivo() {
 
 void decodificar_archivo() {
     size_t input_size;
-    char* input_data = (char*)leer_archivo("decodificacion.txt", &input_size);
+    char* input_data = (char*)leer_archivo("inputTexto.txt", &input_size);
 
     if (!input_data) {
         printf("Error: No se pudo leer decodificacion.txt\n");
